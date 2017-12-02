59668112893908f4907319eb8909e613d729704c
Meiko Rachimow <meiko@users.noreply.github.com>
2017-12-02 22:05:06 +0100

    onclick

--- src/android/GeoNotificationNotifier.java
+++ src/android/GeoNotificationNotifier.java
@@ -4,18 +4,19 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+
+import java.util.Random;
 
 public class GeoNotificationNotifier {
     private NotificationManager notificationManager;
     private Context context;
     private BeepHelper beepHelper;
     private Logger logger;
+    private final Random random = new Random();
 
     public GeoNotificationNotifier(NotificationManager notificationManager, Context context) {
         this.notificationManager = notificationManager;
@@ -26,6 +27,7 @@ public class GeoNotificationNotifier {
 
     public void notify(Notification notification) {
         notification.setContext(context);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, GeofencePlugin.DEFAULT_CHANNEL_ID)
             .setDefaults(NotificationCompat.DEFAULT_VIBRATE | NotificationCompat.DEFAULT_SOUND)
             .setVibrate(notification.getVibrate())
@@ -36,21 +38,19 @@ public class GeoNotificationNotifier {
             .setContentText(notification.getText());
 
         if (notification.openAppOnClick) {
-            String packageName = context.getPackageName();
-            Intent resultIntent = context.getPackageManager()
-                .getLaunchIntentForPackage(packageName);
-            //reuse activity if any
-            resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+            Intent intent = new Intent(context, ClickReceiver.class)
+                    .setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
 
             if (notification.data != null) {
-                resultIntent.putExtra("geofence.notification.data", notification.getDataJson());
+                intent.putExtra("geofence.notification.data", notification.getDataJson());
             }
 
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-            stackBuilder.addNextIntent(resultIntent);
-            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
-                notification.id, PendingIntent.FLAG_UPDATE_CURRENT);
-            mBuilder.setContentIntent(resultPendingIntent);
+            int reqCode = random.nextInt();
+
+            PendingIntent contentIntent = PendingIntent.getActivity(
+                    context, reqCode, intent, FLAG_UPDATE_CURRENT);
+            mBuilder.setContentIntent(contentIntent);
         }
         notificationManager.notify(notification.id, mBuilder.build());
         logger.log(Log.DEBUG, notification.toString());
